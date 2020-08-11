d6d4fb1361c45f9ec5e3f06e48fe71d43351d9d2
Mohd Tarmizi <mohtar@users.noreply.github.com>
2020-08-11 09:59:58 +0800

    Refactor

--- tests/test_log_monitor.py
+++ tests/test_log_monitor.py
@@ -3,17 +3,14 @@ import io
 import os
 
 NUM_TEST_CASES = 3
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
 def test_log_monitor():
     for i in range(NUM_TEST_CASES):
-        with open(
-            os.path.join(os.path.dirname(__file__), 'data/{}.in.txt'.format(i))
-        ) as f:
+        with open(os.path.join(DATA_DIR, '{}.in.txt'.format(i))) as f:
             in_contents = f.read()
-        with open(
-            os.path.join(os.path.dirname(__file__), 'data/{}.out.txt'.format(i))
-        ) as f:
+        with open(os.path.join(DATA_DIR, '{}.out.txt'.format(i))) as f:
             out_contents = f.read()
 
         stdin = io.StringIO(in_contents)
